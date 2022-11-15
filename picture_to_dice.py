c3a4cd061e3907e06fb5587e3841d435c655fb56
declan <dexdavis74@gmail.com>
2022-11-16 10:39:22 +1100

    works agian

--- picture_to_dice.py
+++ picture_to_dice.py
@@ -7,28 +7,28 @@ turtle1.speed(0)
 turtle1.penup()
 
 screen = turtle.Screen()
-screen.setup(100*12, 150*12)
+screen.setup(50*12, 75*12)
 
-image = Image.open("photo-1506794778202-cad84cf45f1d.jpg")
+image = Image.open("IMG_20221115_120137.png")
 imagebw = image.convert("RGB")
 imagebw.save("temp.jpg", quality=1)
 imagebwlow = Image.open("temp.jpg", "r")
 imagebwlow = imagebwlow.convert("RGB")
 #imagebwlow.show()
 width,hight = imagebwlow.size
-size = width/10, hight/10
+size = width/20, hight/20
 imagebwlow.thumbnail(size, Image.Resampling.LANCZOS)
-imagebwlow.show()
+#imagebwlow.show()
 width,hight = imagebwlow.size
 print(hight)
 print(width)
 f = open("output.txt", "w")
 x,y = 0,0
-while y<1800:
+while y<900:
     rgb = imagebwlow.getpixel((x/12,y/12))
     r,g,b = rgb
     a = sum([r,g,b])/3
-    if x >= 1200-12:
+    if x >= 600-12:
         y+=12
         x=0
         f.write("\n")
@@ -38,33 +38,32 @@ while y<1800:
         x+=12
 f.close()
 x,y = 0,0
-while y<1800:
+while y<900:
     rgb = imagebwlow.getpixel((x/12,y/12))
     r,g,b = rgb
     a = sum([r,g,b])/3
-    if x >= 1200-12:
+    if x >= 600-12:
         y+=12
         x=0
     if a <= 3:
-        pass
         x+=12
     elif a <=42:
-        sixSide(x,y)
+        sixSide(x,y,rgb)
         x+=12
     elif a <=42*2:
-        fiveSide(x,y)
+        fiveSide(x,y,rgb)
         x+=12
     elif a <=42*3:
-        fourSide(x,y)
+        fourSide(x,y,rgb)
         x+=12
     elif a <= 42*4:
-        threeSide(x,y)
+        threeSide(x,y,rgb)
         x+=12
     elif a <= 42*5:
-        twoSide(x,y)
+        twoSide(x,y,rgb)
         x+=12
     elif a <= 42*6:
-        oneSide(x,y)
+        oneSide(x,y,rgb)
         x+=12
 
 screen.exitonclick()
--- the_dice.py
+++ the_dice.py
@@ -1,58 +1,59 @@
 import turtle
 
-X_OFFSET = 598
-Y_OFFSET = 897
+X_OFFSET = 298#248
+Y_OFFSET = 350-5#447
 DOT_SIZE = 4
 
 turtle1 = turtle.Turtle(visible=False)
 turtle1.speed('fastest')
 turtle1.penup()
+turtle.colormode(255)
 
-def sixSide(startx, starty):
+def sixSide(startx, starty,rgb):
     turtle1.setposition((0+startx)-X_OFFSET,(-0-starty)+Y_OFFSET)
-    turtle1.dot(DOT_SIZE,'black')
+    turtle1.dot(DOT_SIZE,rgb)
     turtle1.setposition((8+startx)-X_OFFSET,(-0-starty)+Y_OFFSET)
-    turtle1.dot(DOT_SIZE,'black')
+    turtle1.dot(DOT_SIZE,rgb)
     turtle1.setposition((0+startx)-X_OFFSET,(-8-starty)+Y_OFFSET)
-    turtle1.dot(DOT_SIZE,'black')
+    turtle1.dot(DOT_SIZE,rgb)
     turtle1.setposition((8+startx)-X_OFFSET,(-8-starty)+Y_OFFSET)
-    turtle1.dot(DOT_SIZE,'black')
+    turtle1.dot(DOT_SIZE,rgb)
     turtle1.setposition((0+startx)-X_OFFSET,(-4-starty)+Y_OFFSET)
-    turtle1.dot(DOT_SIZE,'black')
+    turtle1.dot(DOT_SIZE,rgb)
     turtle1.setposition((8+startx)-X_OFFSET,(-4-starty)+Y_OFFSET)
-    turtle1.dot(DOT_SIZE,'black')
-def fiveSide(startx, starty):
+    turtle1.dot(DOT_SIZE,rgb)
+def fiveSide(startx, starty,rgb):
     turtle1.setposition((0+startx)-X_OFFSET,(-0-starty)+Y_OFFSET)
-    turtle1.dot(DOT_SIZE,'black')
+    turtle1.dot(DOT_SIZE,rgb)
     turtle1.setposition((8+startx)-X_OFFSET,(-0-starty)+Y_OFFSET)
-    turtle1.dot(DOT_SIZE,'black')
+    turtle1.dot(DOT_SIZE,rgb)
     turtle1.setposition((0+startx)-X_OFFSET,(-8-starty)+Y_OFFSET)
-    turtle1.dot(DOT_SIZE,'black')
+    turtle1.dot(DOT_SIZE,rgb)
     turtle1.setposition((8+startx)-X_OFFSET,(-8-starty)+Y_OFFSET)
-    turtle1.dot(DOT_SIZE,'black')
+    turtle1.dot(DOT_SIZE,rgb)
     turtle1.setposition((4+startx)-X_OFFSET,(-4-starty)+Y_OFFSET)
-    turtle1.dot(DOT_SIZE,'black')
-def fourSide(startx, starty):
+    turtle1.dot(DOT_SIZE,rgb)
+def fourSide(startx, starty,rgb):
     turtle1.setposition((0+startx)-X_OFFSET,(-0-starty)+Y_OFFSET)
-    turtle1.dot(DOT_SIZE,'black')
+    turtle1.dot(DOT_SIZE,rgb)
     turtle1.setposition((8+startx)-X_OFFSET,(-0-starty)+Y_OFFSET)
-    turtle1.dot(DOT_SIZE,'black')
+    turtle1.dot(DOT_SIZE,rgb)
     turtle1.setposition((0+startx)-X_OFFSET,(-8-starty)+Y_OFFSET)
-    turtle1.dot(DOT_SIZE,'black')
+    turtle1.dot(DOT_SIZE,rgb)
     turtle1.setposition((8+startx)-X_OFFSET,(-8-starty)+Y_OFFSET)
-    turtle1.dot(DOT_SIZE,'black')
-def threeSide(startx, starty):
+    turtle1.dot(DOT_SIZE,rgb)
+def threeSide(startx, starty,rgb):
     turtle1.setposition((0+startx)-X_OFFSET,(-0-starty)+Y_OFFSET)
-    turtle1.dot(DOT_SIZE,'black')
+    turtle1.dot(DOT_SIZE,rgb)
     turtle1.setposition((8+startx)-X_OFFSET,(-8-starty)+Y_OFFSET)
-    turtle1.dot(DOT_SIZE,'black')
+    turtle1.dot(DOT_SIZE,rgb)
     turtle1.setposition((4+startx)-X_OFFSET,(-4-starty)+Y_OFFSET)
-    turtle1.dot(DOT_SIZE,'black')
-def twoSide(startx, starty):
+    turtle1.dot(DOT_SIZE,rgb)
+def twoSide(startx, starty,rgb):
     turtle1.setposition((0+startx)-X_OFFSET,(-0-starty)+Y_OFFSET)
-    turtle1.dot(DOT_SIZE,'black')
+    turtle1.dot(DOT_SIZE,rgb)
     turtle1.setposition((8+startx)-X_OFFSET,(-8-starty)+Y_OFFSET)
-    turtle1.dot(DOT_SIZE,'black')
-def oneSide(startx, starty):
+    turtle1.dot(DOT_SIZE,rgb)
+def oneSide(startx, starty,rgb):
     turtle1.setposition((4+startx)-X_OFFSET,(-4-starty)+Y_OFFSET)
-    turtle1.dot(DOT_SIZE,'black')
+    turtle1.dot(DOT_SIZE,rgb)
